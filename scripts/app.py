ea481cd2af42b2657861d65796cf312e96cbaddc
Ethan <ethan.c.chen@gmail.com>
2024-04-20 15:44:37 -0700

    use conversation history in LLM chat

--- scripts/app.py
+++ scripts/app.py
@@ -4,6 +4,15 @@ from llama_index.core.llms import ChatMessage
 from llama_index.llms.fireworks import Fireworks
 
 app = Flask(__name__)
+llm = Fireworks()
+
+
+def compile_conversation_history(ch: list[str]) -> list[ChatMessage]:
+    lst = []
+    for idx, message in enumerate(ch):
+        curr_role = "user" if idx % 2 == 0 else "assistant"
+        lst.append(ChatMessage(role=curr_role, content=message))
+    return lst
 
 
 @app.route("/")
@@ -16,16 +25,20 @@ def get_answer():
     data = request.get_json()
     original_question = data["original_question"]
     conversation_history = data["conversation_history"]
-    # even indices is LLM messages
     # odd indices is LLM messages
+    # even indices is LLM messages
+    # create ChatMessage objects for conversation_history
+    base_system_message = "You are a helpful assistant that assists students with questions in a midterm format. Guide students by addressing what they get wrong if they chose the wrong answer. Do not reveal the true answer of the question. The question the student is working to answer is {original_question}."
     messages = [
         ChatMessage(
             role="system",
-            content="You are a helpful assistant that assists students with questions in a midterm format. Guide students by addressing what they got wrong if they chose the wrong answer. Do not reveal the true answer of the question.",
+            content=base_system_message.format(original_question=original_question),
         ),
-        ChatMessage(role="user", content="What is your name"),
     ]
-    resp = Fireworks().chat(messages).message.content
+    print("ch is", compile_conversation_history)
+    messages.extend(compile_conversation_history(conversation_history))
+    print(messages)
+    resp = llm.chat(messages).message.content
     return jsonify({"message": resp})
 
 
